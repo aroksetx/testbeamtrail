8ffac3bc012aab1d3045e00b54222d6fa6ab28be
aroksetx <aroksetxua@gmail.com>
2019-03-23 00:58:54 +0300

    update gitignore

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-This repository will contain two projects. One with using of Firebase. Another with server api.
+This repository will contain two projects. One with using of Firebase. Another with using of custom server api.
 
 ## Available Scripts
--- server/index.js
+++ server/index.js
@@ -2,19 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
-const low = require('lowdb');
-const FileSync = require('lowdb/adapters/FileSync');
-const adapter = new FileSync('db.json');
-const db = low(adapter);
 const DataBase = require('./DataBase');
+
 DataBase.init();
 app.use(bodyParser());
 app.use(cors());
-
-
-app.get('/', (req, res) => {
-
-});
 
 app.get('/phones', (req, res) => {
     res.send(DataBase.getAllData())
